09adc660d60ca0978dbb68223779d30f77ce651c
CzakoZoltan08 <czakozoltan08@gmail.com>
2024-12-05 14:46:51 +0200

    Log env variable of config

--- src/public/configuration.ts
+++ src/public/configuration.ts
@@ -1,5 +1,3 @@
-import { InternalServerErrorException } from '@nestjs/common/exceptions/internal-server-error.exception';
-import { plainToClass } from 'class-transformer';
 import {
   IsEnum,
   IsInt,
@@ -8,24 +6,32 @@ import {
   validateSync,
 } from 'class-validator';
 
-export const configuration = () => ({
-  NODE_ENV: process.env.NODE_ENV || Environment.Development,
-  PORT: parseInt(process.env.PORT, 10) || 3000,
+import { InternalServerErrorException } from '@nestjs/common/exceptions/internal-server-error.exception';
+import { plainToClass } from 'class-transformer';
 
-  TEST: process.env.NODE_ENV === 'test',
+export const configuration = () => 
+  {
+    console.log('Environment Variables:', process.env);
 
-  DB_HOST: process.env.DB_HOST,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD,
-  DB_DATABASE: process.env.DB_DATABASE,
-  DB_PORT: process.env.DB_PORT,
+    return {
+      NODE_ENV: process.env.NODE_ENV || Environment.Development,
+      PORT: parseInt(process.env.PORT, 10) || 3000,
 
-  JWT_SECRET: process.env.JWT_SECRET,
+      TEST: process.env.NODE_ENV === 'test',
 
-  FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-  FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-});
+      DB_HOST: process.env.DB_HOST,
+      DB_USERNAME: process.env.DB_USERNAME,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_DATABASE: process.env.DB_DATABASE,
+      DB_PORT: process.env.DB_PORT,
+
+      JWT_SECRET: process.env.JWT_SECRET,
+
+      FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+      FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  }
+};
 
 export enum Environment {
   Development = 'development',
