e61dc222b959d7ac89f645fd3515a3c2761ad5c7
CzakoZoltan08 <czakozoltan08@gmail.com>
2024-12-05 12:02:50 +0200

    Use NestExpressApplication instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,29 @@
-import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { NestFactory } from '@nestjs/core';
-import express from 'express';
+import { Handler, NextFunction, Request, Response } from 'express';
 
-const expressApp = express();
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestFactory } from '@nestjs/core';
+
+let server: Handler;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-  app.enableCors();
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors(); // Enable CORS for cross-origin requests
   await app.init();
+
+  // Extract the Express instance
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = (req: Request, res: Response, next: NextFunction) => {
+    expressApp(req, res, next);
+  };
 }
 
 bootstrap();
 
-// Export the Express server for serverless environments
-export default expressApp;
+// Export the Express server as the default export for Vercel
+export default function handler(req: Request, res: Response, next: NextFunction) {
+  if (!server) {
+    throw new Error('Server is not initialized.');
+  }
+  return server(req, res, next);
+}
